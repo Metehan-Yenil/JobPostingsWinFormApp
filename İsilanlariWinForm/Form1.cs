eacf3a47c4d92217ba9aeb5a24736302d747755e
Metehan-Yenil <204410055@ogr.kastamonu.edu.tr>
2023-05-13 17:18:43 +0300

    Proje dosyası ekle.

---
+++ İsilanlariWinForm/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace İsilanlariWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Label label = new Label();
+
+            
+
+
+
+
+
+        }
+
+        private void personelGirisbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void personelKayıtbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
